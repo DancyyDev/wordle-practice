05cda8d451533921d4ffb123626653b2c7f0f04f
Daniel Cha <cha.danieldev@gmail.com>
2022-09-08 16:43:37 -0400

    commit 9/8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Wordle from "./components/wordle.js"
 
 function App() {
+
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    setSolution('hello')
+  }, [setSolution])
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BestFriendWordleReactProject</h1>
+      {solution && <Wordle solution={solution}/>}
     </div>
   );
 }
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+
+const useWordle = (solution) => {
+    //tracks what turn the user is on
+    const [turn, setTurn] = useState(0)
+
+    //tracking what the user is tracking in current guess
+    const [currentGuess, setCurrentGuess] = useState('')
+
+    //tracks guess in an a array with other elements
+    const [guesses, setGuesses] = useState([])
+
+    //tracks past guess in array in a string format
+    const [history, setHistory] = useState(['hello'])
+
+    //is boolean that tells if user is correct or not
+    const [isCorrect, setIsCorrect] = useState(false)
+    
+    //formats guess into array
+    const formatGuess = () => {
+        let solutionArr = [...solution]
+        let formattedGuess = [...currentGuess].map((char) => {
+            return { key: char, color: 'grey' }
+        })
+
+        //find green char
+        formattedGuess.forEach((char, i) => {
+            if(solutionArr[i] === char.key){
+                formattedGuess[i].color = 'green'
+                solutionArr[i] = null
+            }
+        })
+
+        //find yellow char
+        formattedGuess.forEach((char, i) => {
+            if(solutionArr.includes(char.key) && char.color !== 'green')
+            formattedGuess[i].color = 'yellow'
+            solutionArr[solutionArr.indexOf(char.key)] = null
+        })
+
+        return formattedGuess
+    }
+
+    //add new guess
+    //update
+    const addNewGuess = () => {}
+
+    //handle keyup events and track current guesses
+    //when user press enter, add new guess
+    const handleKeyUp = ({ key }) => {
+        if(key === "Enter"){
+            //only add guess if turn is equal to 5
+            if(turn > 5){
+                console.log("No More Turns")
+                return
+            }
+            //do not allow duplicate words
+            if(history.includes(currentGuess)){
+                console.log("You already used that word dummy")
+                return
+            }
+            //word.length === 5
+            if(currentGuess.length !== 5){
+                console.log("Your word needs to be 5 letter's long")
+                return
+            }
+            
+            const formatted = formatGuess()
+            console.log(formatted)
+        }
+
+        if(key === 'Backspace'){
+            setCurrentGuess((prev)=>{
+                return prev.slice(0,-1)
+            })
+            return
+        }
+        if(/^[A-Za-z]$/.test(key)){
+            if(currentGuess.length < 5){
+                setCurrentGuess((prev) => {
+                    return prev + key
+                })
+            }
+        }
+    }
+
+    return {turn, currentGuess, guesses, isCorrect, handleKeyUp}
+}
+
+export default useWordle
